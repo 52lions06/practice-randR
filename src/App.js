86a146bac6489aa8ccaae660bdcfd8cdaaff2eaf
52lions06 <mckelvey.william@yahoo.com>
2017-11-27 15:32:19 -0500

    rm bullets created todo list

--- src/App.js
+++ src/App.js
@@ -10,6 +10,25 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Building a React + Redux App</h1>
         </header>
+        <div className="Todo-App">
+          <form action="#">
+            <input type="text"/>
+            <div className="Todo-List">
+              <ul>
+                <li>
+                  <input type="checkbox"/> Create a Static UI
+                </li>
+                <li>
+                  <input type="checkbox"/> Create an Initial State
+                </li>
+                <li>
+                  <input type="checkbox"/> Use state to render UI
+                </li>
+              </ul>
+              
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
